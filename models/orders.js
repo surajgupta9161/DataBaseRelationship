d97ba1958c62840a630c6b68b189895e88eb232e
surajgupta <suraj666189@gmail.com>
2025-12-04 21:21:09 +0530

    Adding customer with order Id

--- models/orders.js
+++ models/orders.js
@@ -11,11 +11,37 @@ const orderSchema = new Schema({
     price: Number
 })
 
+const custSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Order'
+        }
+    ]
+})
+
 const Order = mongoose.model("Order", orderSchema)
+const Customer = mongoose.model("Customer", custSchema)
 
 const addOrder = async () => {
     const result = await Order.insertMany([{ item: "Samosa", price: 10 }, { item: "Chocolate", price: 40 }, { item: "Chips", price: 20 }])
     console.log(result)
 }
 
-addOrder();
+const addCustomer = async () => {
+    const cust1 = new Customer({
+        name: "Suraj Gupta"
+    })
+    let order1 = await Order.findOne({ item: "Samosa" })
+    let order2 = await Order.findOne({ item: "Chips" })
+
+    cust1.orders.push(order1)
+    cust1.orders.push(order2)
+
+    let result = cust1.save()
+    console.log(result);
+
+}
+addCustomer()
+// addOrder();
